0eed2a557f89128a8e7ec511ec3ac63642078f89
Arturo Filastò <art@fuffa.org>
2015-02-26 14:44:43 +0100

    Remove unneeded code

--- Gruntfile.js
+++ Gruntfile.js
@@ -111,6 +111,10 @@ module.exports = function (grunt) {
                 connect.static('./bower_components')
               ),
               connect().use(
+                '/node_modules',
+                connect.static('./node_modules')
+              ),
+              connect().use(
                 '/app/styles',
                 connect.static('./app/styles')
               ),
@@ -132,6 +136,10 @@ module.exports = function (grunt) {
               connect().use(
                 '/bower_components',
                 connect.static('./bower_components')
+              ),
+              connect().use(
+                '/node_modules',
+                connect.static('./node_modules')
               ),
               connect.static(appConfig.app)
             ];
